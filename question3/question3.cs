3321ed11765bad6015c552725bb0e63f8abd6626
jigyasa1411 <jigyasa.chaudhary@successive.tech>
2021-05-03 22:17:51 +0530

    Question 3 Program

---
+++ question3/question3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace question3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number: ");
+            int num = Convert.ToInt32(Console.ReadLine()); // Entering the number
+
+            Console.WriteLine("Enter the power: ");
+            int power = Convert.ToInt32(Console.ReadLine()); //Entering the exponent
+
+
+            int result = 1;
+
+            // Program to calculate power of a given number.
+            if(power==0 || num==1)
+            {
+                Console.WriteLine("{0} raised to power {1} is {2}",num,power,result);
+            }
+            else
+            {
+                for(int i=1; i<=power; i++)
+                {
+                    result*=num;
+                }
+
+                Console.WriteLine("{0} raised to power {1} is {2}",num,power,result);
+            }
+        }
+    }
+}
