fb607bdd7f601ac1923502cf88f2bb3e30f90632
jigyasa1411 <jigyasa.chaudhary@successive.tech>
2021-05-03 21:36:45 +0530

    Question 2 program

---
+++ question2/question2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace question2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine(); //Taking student's name as input
+
+            Console.WriteLine("Enter your Roll Number:");
+            int rollNo = Convert.ToInt32(Console.ReadLine()); //Taking student's roll no. as input
+
+            Console.WriteLine("Enter your age:");
+            int age = Convert.ToInt32(Console.ReadLine()); // Taking student's age as input
+
+            Console.WriteLine("Enter your class:");
+            int Class = Convert.ToInt32(Console.ReadLine()); // Taking student's class as input
+
+            Console.WriteLine("Enter your University Name:");
+            string universityName = Console.ReadLine(); //Taking student's university name as input
+
+
+            // Printing the details
+            Console.WriteLine("_________________________________________________________________________");
+            Console.WriteLine("Name: "+ name);
+            Console.WriteLine("Roll Number: "+ rollNo);
+            Console.WriteLine("Age: "+ age);
+            Console.WriteLine("Class: "+ Class);
+            Console.WriteLine("University Name: "+ universityName);
+        }
+    }
+}
