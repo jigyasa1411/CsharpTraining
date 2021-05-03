464e8cb01d1673d1f53aabbaffc4e4014a2f2808
jigyasa1411 <jigyasa.chaudhary@successive.tech>
2021-05-03 22:57:59 +0530

    Question 6 Program for library management

---
+++ question6/question6.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace question6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Program for Libraray management system
+            Console.WriteLine("Choose one of the following options respect to kind of book you are looking for: ");
+            Console.WriteLine("c: for computer books");
+            Console.WriteLine("m: for mathematical books");
+            Console.WriteLine("h: for history books");
+            Console.WriteLine("e: for English books");
+
+            string option = Console.ReadLine();
+
+            switch(option)
+            {
+                case "c":
+                    Console.WriteLine("COMPUTER BOOKS");
+                    break;
+                
+                case "m":
+                    Console.WriteLine("MATHEMATICAL BOOKS");
+                    break;
+                
+                case "h":
+                    Console.WriteLine("HISTORY BOOKS");
+                    break;
+
+                case "e":
+                    Console.WriteLine("ENGLISH BOOKS");
+                    break;
+
+                default:
+                    Console.WriteLine("Please choose correct option");
+                    break;
+            }
+
+        }
+    }
+}
