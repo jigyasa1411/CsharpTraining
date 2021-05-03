635e720abe2d3841a9e10098a4e35235bdc86be7
jigyasa1411 <jigyasa.chaudhary@successive.tech>
2021-05-03 22:28:39 +0530

    Question 4 Program

---
+++ question4/question4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace question4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Program for calculation of area of triangle
+
+            Console.WriteLine("Enter the base of triangle:");
+            double TriangleBase = Convert.ToDouble(Console.ReadLine()); // Taking base of the triangle
+
+            Console.WriteLine("Enter height of the triangle:");
+            double TriangleHeight = Convert.ToDouble(Console.ReadLine()); // Taking height of the triangle
+
+            double area = (TriangleBase*TriangleHeight)/2;
+            Console.WriteLine("Area of triangle is : "+ area );
+
+
+
+        
+        }
+    }
+}
