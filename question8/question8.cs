533268fc32a50d3a21102308ebef0e9ad3311626
jigyasa1411 <jigyasa.chaudhary@successive.tech>
2021-05-03 23:31:47 +0530

    Question 8 Program

---
+++ question8/question8.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace question8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Enter the number of rows:");
+           int rows = Convert.ToInt32(Console.ReadLine());
+
+       for(int i=1; i<=rows; i++)      
+       {          
+        for(int j=1; j<=rows-i; j++)      
+        {      
+         Console.Write(" ");      
+        }      
+        for(int k=1;k<=i;k++)      
+        {      
+         Console.Write(k);      
+        }      
+        for(int l=i-1;l>=1;l--)      
+        {      
+        Console.Write(l);      
+        }      
+        Console.Write("\n");      
+       }       
+        }
+    }
+}
