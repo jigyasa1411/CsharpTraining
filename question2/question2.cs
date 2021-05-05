b6eeb26fa8fef4b566dfcc2625e127fd89c9ae24
jigyasa1411 <jigyasa.chaudhary@successive.tech>
2021-05-05 19:47:47 +0530

    Question 2 Update

--- question2/question2.cs
+++ question2/question2.cs
@@ -6,19 +6,19 @@ namespace question2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your name:");
+            Console.WriteLine("Enter your name (In alphabetical format): ");
             string name = Console.ReadLine(); //Taking student's name as input
 
-            Console.WriteLine("Enter your Roll Number:");
+            Console.WriteLine("Enter your Roll Number (In numeric format):");
             int rollNo = Convert.ToInt32(Console.ReadLine()); //Taking student's roll no. as input
 
-            Console.WriteLine("Enter your age:");
+            Console.WriteLine("Enter your age (In numeric format):");
             int age = Convert.ToInt32(Console.ReadLine()); // Taking student's age as input
 
-            Console.WriteLine("Enter your class:");
+            Console.WriteLine("Enter your class (In numeric format):");
             int Class = Convert.ToInt32(Console.ReadLine()); // Taking student's class as input
 
-            Console.WriteLine("Enter your University Name:");
+            Console.WriteLine("Enter your University Name (In alphabetical format):");
             string universityName = Console.ReadLine(); //Taking student's university name as input
 
 
