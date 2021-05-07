fba746d4fbe2538c5767fd396713e1d1b390a41a
jigyasa1411 <jigyasa.chaudhary@successive.tech>
2021-05-07 23:19:59 +0530

    List and Enum Task Program.

---
+++ TaskEnum/TaskEnum.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace TaskEnum
+{
+    enum Months  // Defining Month's enum
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program takes an alphabet as input and return all the month names starting from that alphabet.");
+            choice:
+                Console.WriteLine("Enter an alphabet: ");
+                string alphabet = Console.ReadLine();
+
+            // Checking if input is empty or not.
+            if(string.IsNullOrEmpty(alphabet))
+            {
+                Console.WriteLine("This field can't be empty or null. Enter valid input:");
+                goto choice;
+            }
+
+            
+
+            // As per the given input printing the month's name startimg from that letter.
+            if(alphabet == "A" || alphabet == "a")
+            {
+                Console.WriteLine("Months starting with alphabet {0} : ",alphabet);
+                Console.WriteLine(Months.April);
+                Console.WriteLine(Months.August);
+            }
+
+            else if(alphabet == "D" || alphabet == "d")
+            {
+                Console.WriteLine("Months starting with alphabet {0} : ",alphabet);
+                Console.WriteLine(Months.December);
+            }
+
+            else if(alphabet == "F" || alphabet == "f")
+            {
+                Console.WriteLine("Months starting with alphabet {0} : ",alphabet);
+                Console.WriteLine(Months.February);
+            }
+
+            else if(alphabet == "J" || alphabet == "j")
+            {
+                Console.WriteLine("Months starting with alphabet {0} : ",alphabet);
+                Console.WriteLine(Months.January);
+                Console.WriteLine(Months.June);
+                Console.WriteLine(Months.July);
+
+            }
+
+            else if(alphabet == "M" || alphabet == "m")
+            {
+                Console.WriteLine("Months starting with alphabet {0} : ",alphabet);
+                Console.WriteLine(Months.March);
+                Console.WriteLine(Months.May);
+                
+            }
+
+            else if(alphabet == "N" || alphabet == "n")
+            {
+                Console.WriteLine("Months starting with alphabet {0} : ",alphabet);
+                Console.WriteLine(Months.November);
+                
+            }
+
+            else if(alphabet == "O" || alphabet == "o")
+            {
+                Console.WriteLine("Months starting with alphabet {0} : ",alphabet);
+                Console.WriteLine(Months.October);
+                
+            }
+
+            else if(alphabet == "S" || alphabet == "s")
+            {
+                Console.WriteLine("Months starting with alphabet {0} : ",alphabet);
+                Console.WriteLine(Months.September);
+            }
+            
+            else 
+            {
+                Console.WriteLine("Enter valid choice");
+                goto choice;
+            }
+            
+            
+        }
+    }
+}
---
+++ TaskLists/TaskList.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TaskLists
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+           label:
+                Console.WriteLine("What kind of list you are intending to operate with: ");
+                Console.WriteLine("Press 1 for Integer List.");
+                Console.WriteLine("Press 2 for String List: ");
+                Console.WriteLine("Enter your choice: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("How many elements do you want in your list: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            int count; // specifies count of a particular element in the list.
+
+            if(choice == 1)
+            {
+                List <int> intList = new List<int>();
+                List <int> freqList = new List<int>();
+                Console.WriteLine("Enter the elements in your integer list. After inserting each element press enter key to enter next element.: ");
+                for(int i =0; i<size; i++)
+                {
+                    intList.Add(Convert.ToInt32(Console.ReadLine()));
+                    freqList.Add(-1);
+                }
+
+
+                Console.WriteLine("\n\nOriginal List: ");
+                foreach(int i in intList)
+                {
+                    Console.Write(" "+i);
+                }
+                Console.WriteLine();
+
+               
+                //Sorting
+                intList.Sort();
+                Console.WriteLine("\n\nSorted List: ");
+                foreach(int i in intList)
+                {
+                    Console.Write(" "+i);
+                }
+
+                // Calculating frequency
+                Console.WriteLine("\n\nFrequency: ");
+                
+                // Frequency Counting
+                for(int i=0; i<size; i++)
+                {
+                    count = 1;
+                    for(int j=i+1; j<size; j++)
+                    {
+                        if(intList[i]==intList[j])
+                        {
+                            count++;
+                            freqList[j] = 0;
+                        }
+                    }
+
+                    if(freqList[i]!=0)
+                    {
+                        freqList[i] = count;
+                    }
+                }
+
+                // Frequency Printing
+                for(int i=0; i<size; i++)
+                {
+                    if(freqList[i]!=0)
+                    {
+                        Console.Write("Frequency of {0} is {1}.\n", intList[i], freqList[i]);
+                    }
+
+                }
+                
+            }
+
+            else if(choice == 2)
+            {
+                List <string> stringList = new List<string>();
+                List <int> freqList = new List<int>();
+
+                Console.WriteLine("Enter the elements in your integer list: After inserting each element press enter key to insert next element.: ");
+                for(int i =0; i<size; i++)
+                {
+                    stringList.Add(Console.ReadLine());
+                    freqList.Add(-1); // Insert -1 at each index in the frequency list.
+                }
+
+
+                Console.WriteLine("\n\nOriginal List: ");
+                foreach(string i in stringList)
+                {
+                    Console.Write(" "+i);
+                }
+                Console.WriteLine();
+
+               
+                //Sorting
+                stringList.Sort();
+                Console.WriteLine("\n\nSorted List: ");
+                foreach(string i in stringList)
+                {
+                    Console.Write(" "+i);
+                }
+
+
+                // Calculating Frequency
+                Console.WriteLine("\n\nFrequency: ");
+                // Frequency Counting
+                for(int i=0; i<size; i++)
+                {
+                    count = 1;
+                    for(int j=i+1; j<size; j++)
+                    {
+                        if(stringList[i]==stringList[j])
+                        {
+                            count++;
+                            freqList[j] = 0;
+                        }
+                    }
+
+                    if(freqList[i]!=0)
+                    {
+                        freqList[i] = count;
+                    }
+                }
+
+                
+                // Frequency Printing
+                for(int i=0; i<size; i++)
+                {
+                    if(freqList[i]!=0)
+                    {
+                        Console.Write("Frequency of {0} is {1}.\n", stringList[i], freqList[i]);
+                    }
+
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Enter valid input: ");
+                goto label;
+            }
+
+
+        }
+    }
+}
